eb89d41beabfd958f2117c0e966cfdf198cd6707
luckyJim-dev <gispro@gmail.com>
2020-01-28 21:49:31 +0800

    the const keyword is used to define a variable whose value shouldn't change

---
+++ 02.Variables and Constants/06.letconst.js
@@ -0,0 +1,11 @@
+// the const keyword is used to define a variable whose
+// value shouldn't change
+
+'use strict'
+
+let price = 120.25
+const tax = 0.825
+
+price = 110.21
+
+tax = 1.25          //TypeError: Assignment to constant variable.
