a5ba48c9d571fab43766629c6b40bb8a2ec2cb64
luckyJim-dev <gispro@gmail.com>
2020-01-29 07:17:43 +0800

    Immediately Invoked Function Expression (IIFE) or the Self-Executing Anonymous Function

---
+++ 02.Variables and Constants/10.IIFE.js
@@ -0,0 +1,29 @@
+// var does not have block scope and, in the past, that led developers to use a
+// JavaScript design pattern known as Immediately Invoked Function Expression
+// (IIFE) or the Self-Executing Anonymous Function.
+/** no block scope
+
+'use strict'
+
+var result = 0;
+
+for(var i =0; i<5;i++){
+    var sqrt = Math.sqrt(i);
+    result +=sqrt;
+}
+
+console.log(result)         // 6.146264369941973
+console.log(sqrt)           // 2
+*/
+
+'use strict'
+var result = (function(){
+    var result = 0;
+    for(var i =0;i<5;i++){
+        var sqrt = Math.sqrt(i);
+        result += sqrt;
+    }
+    return result;
+})();
+console.log(result);
+console.log(sqrt);          // ReferenceError: sqrt is not defined
