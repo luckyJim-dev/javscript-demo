a286b9b35c3efe0f6f4d811961f6034de9c216a5
luckyJim-dev <gispro@gmail.com>
2020-01-28 20:53:21 +0800

    let does not permit a varable in a scope to be redefined

---
+++ 02.Variables and Constants/03.no-redefine.js
@@ -0,0 +1,11 @@
+'use strict';
+let max = 100;
+console.log(max);
+
+let max = 200;      // variable max is redefined
+console.log(max)
+
+/***output
+ * Identifier 'max' has already been declared
+ * let does not permit a varable in a scope to be redefined
+ */
