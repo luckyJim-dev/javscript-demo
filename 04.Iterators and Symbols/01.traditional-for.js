f05e52076a5b3e91cd79f292593c1b3a9717a243
luckyJim-dev <gispro@gmail.com>
2020-01-30 14:09:37 +0800

    enhance for statement

---
+++ 04.Iterators and Symbols/01.traditional-for.js
@@ -0,0 +1,4 @@
+const names = ['Sara', 'Jake', 'Pete', 'Mark', 'Jill'];
+for(let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
---
+++ 04.Iterators and Symbols/02.enhance-for.js
@@ -0,0 +1,4 @@
+const names = ['Sara', 'Jake', 'Pete', 'Mark', 'Jill'];
+for(const name of names){
+    console.log(name);
+}
