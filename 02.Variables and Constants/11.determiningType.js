0a43437297e8f7a20eed48e24ba1dadb267c4eb5
luckyJim-dev <gispro@gmail.com>
2020-02-10 20:49:44 +0800

    determining data type

---
+++ 02.Variables and Constants/11.determiningType.js
@@ -0,0 +1,15 @@
+let s = "Nicholas";
+let b = true;
+let i = 22;
+let u;
+let n = null;
+let o = new Object();
+colors = ["red","green","blue"]
+
+console.log(typeof s);                  // string
+console.log(typeof i);                  // number
+console.log(typeof b);                  // boolean
+console.log(typeof u);                  // undefied
+console.log(typeof n);                  // object
+console.log(typeof o);                  // object
+console.log(colors instanceof Array);   // true
